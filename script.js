e4afa218d3e3a69589660ad770dc8e32b1892c40
Vitaliy Arkhipov <odeshop@ya.ru>
2018-02-19 10:46:06 +0300

    rabotaet s preduprezdeniem

--- script.js
+++ script.js
@@ -26,15 +26,27 @@ $(document).ready(function () {
     var month = 0;
 
 // субботы месяца
-    function sub(mon, month) {
+//     function sub(mon, month) {
+//         var c = 0;
+//         for (var i = 1; i <= mon; i++) {
+//             if (moment(date(i, month)).format('dddd') == 'суббота') {
+//                 arraySub[c] = moment(date(i, month)).format('DD');
+//                 $("#tabs-1").append("<p class='qaz ui-button ui-corner-all ui-widget'>" + arraySub[c] + "</p>");
+//                 c++;
+//             }
+//         }
+//         return arraySub;
+//     }
+    function sub(mon, month, h) {
         var c = 0;
         for (var i = 1; i <= mon; i++) {
             if (moment(date(i, month)).format('dddd') == 'суббота') {
                 arraySub[c] = moment(date(i, month)).format('DD');
+                $(h).append("<p class='qaz ui-button ui-corner-all ui-widget'>" + arraySub[c] + "</p>");
                 c++;
             }
         }
-        return arraySub;
+        // return arraySub;
     }
 
 // for (var cnt = 0; cnt < yymm.length; cnt++){
@@ -42,31 +54,65 @@ $(document).ready(function () {
 // }
     $('a').click(function () {
         if ($(this).attr('href') == '#tabs-1') {
-            var yymm = sub(28, 0);
-            var leng = 0;
-            for (var s in yymm) {
-                leng++;
-            }
             $("#tabs-1").empty();
             $("#tabs-1").append("<h3>Выберите субботу</h3>");
-            for (var cnt = 0; cnt < leng; cnt++) {
-                $("#tabs-1").append("<p class='qaz ui-button ui-corner-all ui-widget'>" + arraySub[cnt] + "</p>");
-
-                // console.log(arraySub[cnt]);
-            }
+            var h = "#tabs-1";
+            sub(mon, 0, h);
         } else if ($(this).attr('href') == '#tabs-2') {
-            var yymm = sub(28, 1);
-            var leng = 0;
-            for (var s in yymm) {
-                leng++;
-            }
             $("#tabs-2").empty();
             $("#tabs-2").append("<h3>Выберите субботу</h3>");
-            for (var cnt = 0; cnt < leng; cnt++) {
-                $("#tabs-2").append("<p class='qaz ui-button ui-corner-all ui-widget'>" + arraySub[cnt] + "</p>");
-
-                // console.log(arraySub[cnt]);
-            }
+            var h = "#tabs-2";
+            sub(mon, 1, h);
+        } else if ($(this).attr('href') == '#tabs-3') {
+            $("#tabs-3").empty();
+            $("#tabs-3").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-3";
+            sub(mon, 2, h);
+        } else if ($(this).attr('href') == '#tabs-4') {
+            $("#tabs-4").empty();
+            $("#tabs-4").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-4";
+            sub(mon, 3, h);
+        } else if ($(this).attr('href') == '#tabs-5') {
+            $("#tabs-5").empty();
+            $("#tabs-5").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-5";
+            sub(mon, 4, h);
+        } else if ($(this).attr('href') == '#tabs-6') {
+            $("#tabs-6").empty();
+            $("#tabs-6").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-6";
+            sub(mon, 5, h);
+        } else if ($(this).attr('href') == '#tabs-7') {
+            $("#tabs-7").empty();
+            $("#tabs-7").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-7";
+            sub(mon, 6, h);
+        } else if ($(this).attr('href') == '#tabs-8') {
+            $("#tabs-8").empty();
+            $("#tabs-8").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-8";
+            sub(mon, 7, h);
+        } else if ($(this).attr('href') == '#tabs-9') {
+            $("#tabs-9").empty();
+            $("#tabs-9").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-9";
+            sub(mon, 8, h);
+        } else if ($(this).attr('href') == '#tabs-10') {
+            $("#tabs-10").empty();
+            $("#tabs-10").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-10";
+            sub(mon, 9, h);
+        } else if ($(this).attr('href') == '#tabs-11') {
+            $("#tabs-11").empty();
+            $("#tabs-11").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-11";
+            sub(mon, 10, h);
+        } else if ($(this).attr('href') == '#tabs-12') {
+            $("#tabs-12").empty();
+            $("#tabs-12").append("<h3>Выберите субботу</h3>");
+            var h = "#tabs-12";
+            sub(mon, 11, h);
         }
     });
 
